ff135b334f146e74f0fe4b8561b9b2d6251bcf79
Chang&Juju <changliuharry@gmail.com>
2024-06-13 19:59:51 +0800

    handled undifined pages

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,46 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+
 export default authMiddleware({
   publicRoutes: [
     '/',
     '/api/webhook',
-    '/sign-up',  // Ensure the sign-up page is also public
-    '/sign-in',   // Ensure the sign-in page is also public
-    '/dashboard'
+    '/sign-up',
+    '/sign-in',
+    '/dashboard',
   ],
 
   afterAuth(auth, req) {
+    console.log(`************************************************************`);
     console.log(`Auth middleware triggered for URL: ${req.url}`);
     console.log(`User ID: ${auth.userId}, isPublicRoute: ${auth.isPublicRoute}`);
+    console.log(`Request Headers: ${JSON.stringify(req.headers, null, 2)}`);
 
-    if (auth.userId && auth.isPublicRoute) {
-      let path = '/dashboard'
-      const dashboard = new URL(path, req.url);
-      console.log(`Redirecting authenticated user to: ${dashboard}`);
-      return NextResponse.redirect(dashboard);
+    const isAuthenticated = auth.userId !== null;
+    let returnUrl = req.url || '/';
+    if (returnUrl.includes('undefined')) {
+      returnUrl = `${appUrl}/dashboard`;
     }
 
-    if (!auth.userId && !auth.isPublicRoute) {
-      console.log(`Redirecting unauthenticated user to sign-in`);
-      return redirectToSignIn({
-        returnBackUrl: req.url,
-      });
+    if (isAuthenticated && req.nextUrl.pathname === '/') {
+      // Redirect authenticated user to the dashboard
+      const dashboardUrl = new URL('/dashboard', appUrl).toString();
+      console.log(`Redirecting authenticated user to: ${dashboardUrl}`);
+      return NextResponse.redirect(dashboardUrl);
+    }
+
+    if (!isAuthenticated && !auth.isPublicRoute) {
+      const signInUrl = new URL('/sign-in', appUrl);
+      signInUrl.searchParams.set('redirect_url', returnUrl);
+      console.log(`Redirecting unauthenticated user to sign-in with returnBackUrl: ${returnUrl}`);
+      return NextResponse.redirect(signInUrl.toString());
     }
 
     console.log(`User ID: ${auth.userId}, URL: ${req.url}`);
+    console.log(`------------------------------------------------------------`);
+    return NextResponse.next();
   }
 });
 
