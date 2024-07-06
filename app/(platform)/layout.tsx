662180ca2b74810bb32bbbf7f5ff9656ca6acec6
samyang <416650596@gmail.com>
2024-07-06 23:05:36 +0800

    Hot fix eighteen confirm page

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,24 +1,12 @@
-"use client";
-
 import { ClerkProvider } from "@clerk/nextjs";
-import { useEighteenConfirm } from "@/hooks/use-eighteen-confirm";
-import { EighteenConfirm } from "@/components/eighteen-confirm";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
-  const { isConfirmed, onConfirm } = useEighteenConfirm();
-
   return (
-    <>
-      {isConfirmed ? (
-        <ClerkProvider afterSignInUrl="/dashboard" afterSignUpUrl="/dashboard">
-          {/* <ViewTransitions> */}
-          {children}
-          {/* </ViewTransitions> */}
-        </ClerkProvider>
-      ) : (
-        <EighteenConfirm onConfirm={onConfirm}/>
-      )}
-    </>
+    <ClerkProvider afterSignInUrl="/dashboard" afterSignUpUrl="/dashboard">
+      {/* <ViewTransitions> */}
+      {children}
+      {/* </ViewTransitions> */}
+    </ClerkProvider>
   );
 };
 
